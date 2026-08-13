06f1b9ca36db82c95f7c52314ed52ce5cddaaf21
ShubhamBhaskar1307 <45693332+TestGithubAccount987@users.noreply.github.com>
2026-08-14 00:09:11 +0530

    code improvement

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,13 +1,20 @@
-# # Guess The N
-# 1) Start a Infinate loop
-# 2) Accept int 
-# 3) Verify if number == 7
-# 4) in case of True print Won else try again/
+# Guess The Number
+# 1) Attempt = 5
+# 2) Start a loop 
+# 3) if Attempt == 0: Print(Lost)
+# 4) Accept int 
+# 5) Verify if number == 7
+# 6) in case of True print Won else try again 
+# 7) after 5 tries declare Lost
 
-while True:
+attempt = 5
+while True:    
+    if attempt == 0:
+        print("Lost")
+        break
     guess = int(input("Guess the Number : "))
     if guess == 7:
         print("Won")
-        break
-    else:
-        print("try Again")
+        break   
+    attempt -= 1
+    print(f"try Again,attempts remaining {attempt}")        
